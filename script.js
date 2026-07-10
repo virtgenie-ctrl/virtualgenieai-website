8b96817599d70d23740fa094003b32eeb1f5a82e
virtgenie-ctrl <virtgenie@gmail.com>
2026-07-10 23:21:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ async function sendMessage() {
 
         const data = await response.json();
 
-        console.log(data);
+        console.log(JSON.stringify(data, null, 2));
 
         const reply =
             data.candidates?.[0]?.content?.parts?.[0]?.text ||
