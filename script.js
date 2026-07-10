6d711b4d88a652da7f8dc4fcf97d5009a5760140
virtgenie-ctrl <virtgenie@gmail.com>
2026-07-10 18:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+// Scroll reveal
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+document.querySelectorAll("section").forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+// Mobile menu
+const menuToggle = document.getElementById("menuToggle");
+const desktopNav = document.querySelector(".desktop-nav");
+
+console.log(typeof menuToggle);
+console.log(typeof desktopNav);
+
+if (menuToggle && desktopNav) {
+    menuToggle.addEventListener("click", () => {
+        console.log("clicked");
+        desktopNav.classList.toggle("active");
+    });
+}
